f12072710c5446f7c8fe5a8d8231dc7bc6c6d48e
Eva <nikolaeva.eva18@gmail.com>
2019-12-04 11:02:20 +0100

    Equipment

--- Model_Implementation/src/Equipment.java
+++ Model_Implementation/src/Equipment.java
@@ -22,4 +22,81 @@ public class Equipment
             this.numberOfTables = numberOfTables;
         }
     }
+
+    public boolean getHDMI()
+    {
+        return isHDMI;
+    }
+
+    public boolean getVGA()
+    {
+        return isVGA;
+    }
+
+    public boolean getDisplayPort()
+    {
+        return isDisplayPort;
+    }
+
+    public boolean getProjector()
+    {
+        return isProjector;
+    }
+
+    public int getNumberOfChairs()
+    {
+        return numberOfChairs;
+    }
+
+    public int getNumberOfTables()
+    {
+        return numberOfTables;
+    }
+
+    public void setHDMI(boolean HDMI)
+    {
+        isHDMI = HDMI;
+    }
+
+    public void setVGA(boolean VGA)
+    {
+        isVGA = VGA;
+    }
+
+    public void setDisplayPort(boolean displayPort)
+    {
+        isDisplayPort = displayPort;
+    }
+
+    public void setProjector(boolean projector)
+    {
+        isProjector = projector;
+    }
+
+    public void setNumberOfChairs(int numberOfChairs)
+    {
+        this.numberOfChairs = numberOfChairs;
+    }
+
+    public void setNumberOfTables(int numberOfTables)
+    {
+        this.numberOfTables = numberOfTables;
+    }
+
+    public String toString()
+    {
+        return "HDMI: " + isHDMI + ", VGA: " + isVGA + ", Display port: " + isDisplayPort
+                + ", Projector: " + isDisplayPort + ", Number of chairs: " + numberOfChairs
+                + ", Number of tables: " + numberOfTables;
+    }
+
+    public boolean equals(Object obj)
+    {
+        if(!(obj instanceof Equipment))
+            return false;
+        Equipment other = (Equipment) obj;
+        return isHDMI == other.isHDMI && isVGA == other.isVGA && isDisplayPort == other.isDisplayPort
+                && isProjector == other.isProjector && numberOfChairs == other.numberOfChairs &&
+                numberOfTables == other.numberOfTables;
+    }
 }
