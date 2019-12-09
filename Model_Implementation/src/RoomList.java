fdfc236d7826d6c7f7b6e9868a2d8dd33db63d1b
Eva Nikolaeva <nikolaeva.eva18@gmail.com>
2019-12-09 09:43:35 +0100

    RoomList half-ready

---
+++ Model_Implementation/src/RoomList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class RoomList
+{
+    private ArrayList<Room> rooms;
+
+    public RoomList()
+    {
+        this.rooms = new ArrayList<>();
+    }
+
+    public void addRoom(Room room)
+    {
+        rooms.add(room);
+    }
+
+
+    public void removeRoom(Room room)
+    {
+        rooms.remove(room);
+    }
+
+    public Room getRoomByNumber(Room room)
+    {
+        for(int i = 0; i < rooms.size(); i++)
+        {
+            if(rooms.get(i).equals(room))
+            {
+                return room;
+            }
+        }
+        return null;
+    }
+
+    
+}
