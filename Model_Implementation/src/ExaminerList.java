8181af61aa9220fafc1c9ba9ffadec050267c592
Sabina <elenasabina@ymail.com>
2019-12-09 09:38:36 +0100

    Examiner class

---
+++ Model_Implementation/src/ExaminerList.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class ExaminerList
+{
+    private ArrayList<Examiner> examiners;
+
+    public ExaminerList()
+    {
+        this.examiners = new ArrayList<>();
+    }
+
+    public void addExaminer(Examiner examiner)
+    {
+        examiners.add(examiner);
+    }
+
+    public void removeExaminer(Examiner examiner)
+    {
+        examiners.remove(examiner);
+    }
+
+    public int getNumbersOfExaminer()
+    {
+        return examiners.size();
+    }
+
+    public ArrayList<Examiner> getExaminersByCourse(String course)
+    {
+        ArrayList<Examiner> examinersByCourse = new ArrayList<>();
+        for (int i = 0; i < examiners.size(); i++)
+        {
+            if (examiners.get(i).getCourse().equals(course))
+            {
+                examinersByCourse.add(examiners.get(i));
+            }
+        }
+        return examinersByCourse;
+    }
+
+    public ArrayList<Examiner> seeAvailableExaminers(boolean availability)
+    {
+        ArrayList<Examiner> availableExaminers = new ArrayList<>();
+        for (int i = 0; i < examiners.size(); i++)
+        {
+            if (examiners.get(i).isAvailable() == true)
+            {
+                availableExaminers.add(examiners.get(i));
+            }
+        }
+        return availableExaminers;
+    }
+
+    public String toString()
+    {
+        for (int i = 0; i < examiners.size(); i++)
+        {
+            return examiners.get(i).toString() + "/n";
+        }
+        return null;
+    }
+}
