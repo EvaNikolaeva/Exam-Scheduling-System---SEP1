3bc4e3d40c6289d03df93d36541d30aec53995a2
Sabina <elenasabina@ymail.com>
2019-12-18 11:57:14 +0100

    project report

--- src/Model/Course.java
+++ src/Model/Course.java
@@ -1,25 +1,49 @@
 package Model;
 
+/**
+ * The type Course.
+ */
 public class Course
 {
     private int semester;
     private String name;
     private String type;
     private int numberOfStudents;
+    /**
+     * The constant LEGAL_COURSES.
+     */
     public static final String LEGAL_COURSES[] = {"SDJ1X", "SDJ1Y", "SDJ1Z", "RWD1X", "RWD1Y", "RWD1Z", "MSE1X", "MSE1Y",
             "MSE1Z", "SEP1X", "SEP1Y", "SEP1Z", "SDJ2", "SDJ3X", "SDJ3Y", "SEP2", "NES1X", "NES1Y", "DNP1X", "DNP1Y", "DBS1"};
 
+    /**
+     * Instantiates a new Course.
+     *
+     * @param name             the name
+     * @param type             the type
+     * @param numberOfStudents the number of students
+     * @param semester         the semester
+     */
     public Course(String name, String type, int numberOfStudents, int semester)
     {
         this.semester = semester;
         setCourse(name, type, numberOfStudents);
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
     public void setName(String name)
     {
         boolean illegal = true;
@@ -40,36 +64,74 @@ public class Course
         }
     }
 
+    /**
+     * Gets type.
+     *
+     * @return the type
+     */
     public String getType()
     {
         return type;
     }
 
+    /**
+     * Sets type.
+     *
+     * @param type the type
+     */
     public void setType(String type)
     {
         this.type = type;
     }
 
+    /**
+     * Gets number of students.
+     *
+     * @return the number of students
+     */
     public int getNumberOfStudents()
     {
         return numberOfStudents;
     }
 
+    /**
+     * Sets number of students.
+     *
+     * @param numberOfStudents the number of students
+     */
     public void setNumberOfStudents(int numberOfStudents)
     {
         this.numberOfStudents = numberOfStudents;
     }
 
+    /**
+     * Gets semester.
+     *
+     * @return the semester
+     */
     public int getSemester()
     {
         return semester;
     }
 
+    /**
+     * Sets semester.
+     *
+     * @param semester the semester
+     */
     public void setSemester(int semester)
     {
         this.semester = semester;
     }
 
+    /**
+     * Sets course.
+     *
+     * @param name             the name
+     * @param type             the type
+     * @param numberOfStudents the number of students
+     * @throws IllegalArgumentException the illegal argument exception
+     */
     public void setCourse(String name, String type, int numberOfStudents) throws IllegalArgumentException
     {
         boolean illegal = true;
